64b86520f85547b2cab2052fa6f7c72cb65cba75
asepsuhardy14 <42445010+asepsuhardy14@users.noreply.github.com>
2021-03-11 11:13:40 +0700

    Create install-node.sh

---
+++ install-node.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "installing docker"
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+if [ "$(lsb_release -cs)" == "bionic" ] ; then
+  sudo apt-get update && sudo apt-get install -y containerd.io=1.2.13-2 docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+else
+ apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+fi
+
+echo "installing kubeadm and kubectl"
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y -q kubeadm=1.18.12-00 kubelet=1.18.12-00 kubectl=1.18.12-00
